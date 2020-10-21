2430c3b83515267c9f0437dcdabc968b90c3d5f5
Muheeth10 <64929596+Muheeth10@users.noreply.github.com>
2020-10-21 22:58:39 +0530

    Update Experiment_7.java

--- Experiment_7.java
+++ Experiment_7.java
@@ -6,13 +6,15 @@ public class Experiment_7
 		int count=0;
 		int ar1[] = new int[1000];
 		int ar2[] = new int[1000];
-		System.out.println("pair od dices with same number are");
+		System.out.println("pair of dices with same number are");
 		for(int i=0;i<1000;i++){
 		    ar1[i]=rn.nextInt(6);
 		    ar2[i]=rn.nextInt(6);
-		    System.out.print("( "+ar1[i]+","+ar2[i]+" )/t");
-		    if(ar1[i]==ar2[i])
+		    
+		    if(ar1[i]==ar2[i]){
+			System.out.print("( "+ar1[i]+","+ar2[i]+" )\t");
 		        count++;
+		    }
 		}
 		System.out.println("Number of doubles = "+count);
 	}
