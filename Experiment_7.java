46cdb8cf6c9f78f0250afb287451ea203fe7b4ea
Muheeth10 <64929596+Muheeth10@users.noreply.github.com>
2020-10-21 23:03:54 +0530

    Update Experiment_7.java

--- Experiment_7.java
+++ Experiment_7.java
@@ -8,8 +8,8 @@ public class Experiment_7
 		int ar2[] = new int[1000];
 		System.out.println("pair of dices with same number are");
 		for(int i=0;i<1000;i++){
-		    ar1[i]=rn.nextInt(6);
-		    ar2[i]=rn.nextInt(6);
+		    ar1[i]=1+rn.nextInt(6);
+		    ar2[i]=1+rn.nextInt(6);
 		    
 		    if(ar1[i]==ar2[i]){
 			System.out.print("( "+ar1[i]+","+ar2[i]+" )\t");
